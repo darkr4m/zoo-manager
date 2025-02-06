72db4791904900432378fcfa0765ea16d028d322
max <hmwright404@gmail.com>
2025-02-06 11:34:35 -0600

    clone and pseudocode

---
+++ zoo_manager.py
@@ -0,0 +1,36 @@
+"""
+Zoo animal Manager
+Classes:
+    - Animal (parent class)
+        - Mammal
+            - Primate
+            - Marsupial
+        - Bird
+        - Reptile
+    Aviary
+    ReptileEnclosure
+
+    Animal - class
+        name - string - Represents the name of the animal
+        species - string - Repesents the species of the animal (scientific name)
+        #speak
+            returns string representing sound the animal makes
+    Mammal - class (animal)
+        #give_birth
+            prints message indicating that the mammal has given birth
+    Primate - class (mammal)
+        #climb_trees
+            prints message that the primate is climbing trees
+    Marsupial - class (mammal)
+        #carry_baby
+            prints message that the marsupial is carrying its baby
+    Bird - class (animal)
+        wingspan - int - represents the wingspan of the bird in meters
+    Reptile - class (animal)
+        #bask_in_sun
+            prints a message indicating that the reptile is basking in the sun
+    Aviary - class
+        birds - [Bird] - list of bird instances
+    ReptileEnclosure - class
+        repriles - [Reptile] - list of reptile instances
+"""
